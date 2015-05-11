aa36573152d0ca1c370e6abbcfb699fdea865201
Renjith <renjith@sayonetech.com>
2015-05-11 19:26:31 +0530

    changes

--- notifications/models.py
+++ notifications/models.py
@@ -201,6 +201,7 @@ class Notification(models.Model):
             self.mark_as_pushed()
 
         android_device_list = GCMDevice.objects.filter(user=self.recipient)
+        print "android -----------"
         print self.recipient
         print android_device_list
         if android_device_list:
