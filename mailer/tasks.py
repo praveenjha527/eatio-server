5eb053e5947832b9760909e57d0a1988fe1f4169
Renjith <renjith@sayonetech.com>
2015-05-11 11:39:20 +0530

    added periodic tasks

--- mailer/tasks.py
+++ mailer/tasks.py
@@ -1,10 +1,19 @@
 from __future__ import absolute_import
 from datetime import timedelta
 
-from celery.task import PeriodicTask, task
+from celery.task import PeriodicTask
 from mailer.engine import send_all
 
 
-@task()
-def send_queued_emails(*args, **kwargs):
-    send_all()
+class SendEmailTask(PeriodicTask):
+    """
+    A periodic task that send email notification.
+    # this will run every 30 seconds
+    # send all emails in the mailer queue
+    """
+    run_every = timedelta(seconds=30)
+
+    def run(self, **kwargs):
+        print "sending email"
+        send_all()
+        return True
